4d516515afd45f1488cf28308ef1f710fa764ce1
hakimhassani97 <hakimhassani97@gmail.com>
2019-12-25 16:47:13 +0100

    fixed difference function

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import cv2
 import numpy as np
 import math
+import time
 
+#functions
 def distance(x1,y1,img1,x2,y2,img2):
     err=0.0
-    for i in range(0,hWin):
-        for j in range(0,wWin):
-            err+=math.pow(int(img1[y1-i//2,x1-j//2])-int(img2[y2-i//2,x2-j//2]),2)
+    for i in range(0,hWindow):
+        for j in range(0,wWindow):
+            if y1-i//2>=0 and x1-j//2>=0 and y2-i//2>=0 and x2-j//2>=0:
+                err+=math.pow(int(img1[y1-i//2,x1-j//2])-int(img2[y2-i//2,x2-j//2]),2)
             pass
     return math.sqrt(err)
 
-l='im0.png'
-r='im1.png'
+#data
+l='data/im0.png'
+r='data/im1.png'
 # l='0.png'
 # r='1.png'
 
@@ -20,9 +24,11 @@ iml = cv2.imread(l,cv2.IMREAD_GRAYSCALE)
 imr = cv2.imread(r,cv2.IMREAD_GRAYSCALE)
 hl,wl=np.shape(iml)
 hr,wr=np.shape(imr)
-wWin,hWin=16,16
+wWindow,hWindow=16,16
 
+start_time = time.time()
 d=distance(18,0,iml,0,0,imr)
+print("--- %s seconds ---" % (time.time() - start_time))
 print('err='+str(d))
 
 cv2.namedWindow('image : '+l)
